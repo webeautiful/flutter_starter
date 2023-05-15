2f8a4d9c02e700f975dcaab0de240e1344ab3382
Xiongfs <121023254@qq.com>
2023-05-15 16:43:41 +0800

    feat: local assets config

---
+++ lib/assets.dart
@@ -0,0 +1,4 @@
+class ImagePaths {
+  static String root = 'assets/images';
+  static String appLogo = '$root/app-logo.png';
+}
--- lib/page/home/page.dart
+++ lib/page/home/page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_starter/assets.dart';
 import 'package:flutter_starter/utils/log_util.dart';
 
 class HomePage extends StatelessWidget {
@@ -9,12 +10,17 @@ class HomePage extends StatelessWidget {
     logI('[debug]build home page');
     return Container(
       color: Colors.greenAccent,
-      child: const SafeArea(
+      child: SafeArea(
         child: Column(
           children: [
-            SearchBar(
+            const SearchBar(
               hintText: 'Search...',
             ),
+            Image.asset(
+              ImagePaths.appLogo,
+              width: 100,
+              height: 100,
+            )
           ],
         ),
       ),
